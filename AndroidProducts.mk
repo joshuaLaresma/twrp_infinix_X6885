cf8c8b4f1f6d09df0b9e95c7e8e4e29c6def16fb
joshuaLaresma <158560563+joshuaLaresma@users.noreply.github.com>
2026-08-08 23:06:30 +0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/twrp_X6885.mk
+
+COMMON_LUNCH_CHOICES := \
+	twrp_X6885-eng \
+	twrp_X6885-userdebug \
+	twrp_X6885-user
